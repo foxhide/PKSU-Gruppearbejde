8d41aeaf38c1fa633479e8388082acf1bcbfc6ce
Stephan Lorenzen <stephan.lorenzen@gmail.com>
2013-03-26 15:59:06 +0100

    Beginning work on the view check for calendar.

--- CalendarApplication/CalendarApplication/Models/Calendar/ViewCheckModel.cs
+++ CalendarApplication/CalendarApplication/Models/Calendar/ViewCheckModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CalendarApplication.Models.User;
 
 namespace CalendarApplication.Models.Calendar
 {
     public class ViewCheckModel
     {
-        public int Groups { set; get; }
+        public int Month { set; get; }
+        public int Year { set; get; }
+        public List<GroupModel> GroupsAvailable { set; get; }
+        public List<GroupModel> GroupsSelected { set; get; }
         public int Eventtypes { set; get; }
     }
 }
---
+++ CalendarApplication/CalendarApplication/Models/User/GroupModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalendarApplication.Models.User
+{
+    public class GroupModel
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(this, obj)) return true;
+
+            if (obj == null) return false;
+
+            GroupModel otherGroup = (GroupModel)obj;
+            return otherGroup.ID == ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
+    }
+}
