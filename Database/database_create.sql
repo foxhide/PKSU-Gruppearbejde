3d8b8e7b4606a7fa008a1ecaa4a0545c9e88b539
Johan V. Damgaard <pwr273@alumni.ku.dk>
2013-03-28 15:19:22 +0100

    small database changes

--- Database/database_create.sql
+++ Database/database_create.sql
@@ -16,17 +16,19 @@ CREATE  TABLE IF NOT EXISTS `pksudb`.`users` (
   `userName` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
   `realName` VARCHAR(45) NOT NULL ,
+  `admin` TINYINT(1) NULL DEFAULT 0 ,
   `email` VARCHAR(45) NULL DEFAULT NULL ,
-  `active` TINYINT(1) NULL DEFAULT NULL ,
-  `needsApproval` TINYINT(1) NULL DEFAULT NULL ,
+  `active` TINYINT(1) NULL DEFAULT 0 ,
+  `needsApproval` TINYINT(1) NULL DEFAULT 1 ,
   PRIMARY KEY (`userId`) ,
   UNIQUE INDEX `userName_UNIQUE` (`userName` ASC) )
 AUTO_INCREMENT = 1
 COMMENT = 'Table for user logins, passwords and information.\n\nuserId: U /* comment truncated */ /*nique userid for each user (primary key). 
-userName: Unique login/screen name tied to userId. Max 45 chars.
-password: Password tied to a userId. max 45 chars.
-realname: real name of the user. Max 45 chars.
-email: user email address. max 45 chars.
+userName: Unique login/screen name tied to userId. Required, 45 chars.
+password: Password tied to a userId. Required, 45 chars.
+realname: real name of the user. Required, 45 chars.
+admin: whether or not this user has admin status.
+email: user email address. Not required, 45 chars.
 active: whether the user is active, inactive users cannot login.
 needsApproval: whether the user needs initial approval from an administrator.*/';
 
@@ -39,12 +41,10 @@ DROP TABLE IF EXISTS `pksudb`.`eventtypes` ;
 CREATE  TABLE IF NOT EXISTS `pksudb`.`eventtypes` (
   `eventTypeId` INT NOT NULL AUTO_INCREMENT ,
   `eventTypeName` VARCHAR(45) NOT NULL ,
-  `dbTableName` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`eventTypeId`) )
 AUTO_INCREMENT = 1
 COMMENT = 'Table for event types with names.\n\neventTypeId: Primary key. /* comment truncated */ /*
-eventTypeName: name of a event type
-dbTableName: name of the table in the database.*/';
+eventTypeName: name of a event type*/';
 
 
 -- -----------------------------------------------------
@@ -59,8 +59,8 @@ CREATE  TABLE IF NOT EXISTS `pksudb`.`events` (
   `eventName` VARCHAR(45) NOT NULL ,
   `eventStart` DATETIME NOT NULL ,
   `eventEnd` DATETIME NOT NULL ,
-  `visible` TINYINT(1) NULL DEFAULT NULL ,
-  `state` INT NULL DEFAULT NULL ,
+  `visible` TINYINT(1) NULL DEFAULT 1 ,
+  `state` INT NULL DEFAULT 0 ,
   PRIMARY KEY (`eventId`) ,
   INDEX `userId_idx` (`userId` ASC) ,
   INDEX `eventTypeId_idx` (`eventTypeId` ASC) ,
@@ -94,14 +94,7 @@ CREATE  TABLE IF NOT EXISTS `pksudb`.`files` (
   `fileId` INT NOT NULL AUTO_INCREMENT ,
   `fileName` VARCHAR(45) NOT NULL ,
   `pathToFile` VARCHAR(150) NOT NULL ,
-  `eventId` INT NOT NULL ,
-  PRIMARY KEY (`fileId`) ,
-  INDEX `event_idx` (`eventId` ASC) ,
-  CONSTRAINT `fileUsedInEvent`
-    FOREIGN KEY (`eventId` )
-    REFERENCES `pksudb`.`events` (`eventId` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`fileId`) )
 AUTO_INCREMENT = 1
 COMMENT = 'Table for files.\n\nfileId: unique primary key for files.\nfile /* comment truncated */ /*Name: name of the file, e.g. mytextfile.txt.
 pathToFile: path to file (duh), e.g. /root/myfolder/mytextfile.txt.
@@ -187,6 +180,7 @@ DROP TABLE IF EXISTS `pksudb`.`groupmembers` ;
 CREATE  TABLE IF NOT EXISTS `pksudb`.`groupmembers` (
   `groupId` INT NOT NULL ,
   `userId` INT NOT NULL ,
+  `groupLeader` TINYINT(1) NULL DEFAULT 0 ,
   PRIMARY KEY (`groupId`, `userId`) ,
   INDEX `membergroupid_idx` (`groupId` ASC) ,
   INDEX `memberuserid_idx` (`userId` ASC) ,
@@ -203,6 +197,7 @@ CREATE  TABLE IF NOT EXISTS `pksudb`.`groupmembers` (
 ENGINE = InnoDB
 COMMENT = 'A table describing which users belong to which groups.\n\ngrou /* comment truncated */ /*pId: foreign key referencing groups table.
 userId: foreign key referencing users table.
+groupLeader: whether this user is one of the group leaders.
 primary key is both together.*/';
 
 
@@ -249,53 +244,31 @@ primary key is both together.*/';
 
 
 -- -----------------------------------------------------
--- Table `pksudb`.`fieldtypes`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `pksudb`.`fieldtypes` ;
-
-CREATE  TABLE IF NOT EXISTS `pksudb`.`fieldtypes` (
-  `fieldId` INT NOT NULL AUTO_INCREMENT ,
-  `fieldtype` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`fieldId`) )
-AUTO_INCREMENT = 1
-COMMENT = 'Table for field types. Any inserts beyond the ones from init /* comment truncated */ /*ialization will require changing the webcode.
-
-fieldId: Primary key.
-fieldType: Name of the field type.*/';
-
-
--- -----------------------------------------------------
 -- Table `pksudb`.`eventtypefields`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `pksudb`.`eventtypefields` ;
 
 CREATE  TABLE IF NOT EXISTS `pksudb`.`eventtypefields` (
   `eventTypeId` INT NOT NULL ,
-  `dbFieldName` VARCHAR(45) NOT NULL ,
+  `fieldName` VARCHAR(45) NOT NULL ,
   `fieldDescription` VARCHAR(100) NOT NULL ,
-  `requiredField` TINYINT(1) NULL DEFAULT NULL ,
-  `fieldTypeId` INT NOT NULL ,
-  PRIMARY KEY (`eventTypeId`, `dbFieldName`) ,
+  `requiredField` TINYINT(1) NULL DEFAULT 0 ,
+  `fieldType` INT NULL ,
+  PRIMARY KEY (`eventTypeId`, `fieldName`) ,
   INDEX `eventtypenameid_idx` (`eventTypeId` ASC) ,
-  INDEX `fieldtypenameid_idx` (`fieldTypeId` ASC) ,
   CONSTRAINT `eventtypenameid`
     FOREIGN KEY (`eventTypeId` )
     REFERENCES `pksudb`.`eventtypes` (`eventTypeId` )
     ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fieldtypenameid`
-    FOREIGN KEY (`fieldTypeId` )
-    REFERENCES `pksudb`.`fieldtypes` (`fieldId` )
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 COMMENT = 'Table for which fields events use and whether they are requi /* comment truncated */ /*red fields.
 
 eventTypeId: foreign key referencing eventtypenames. cannot be null.
-dbFieldName: name of the field in the database. cannot be null.
+fieldName: name of the field in the database. cannot be null.
 fieldDescription: description of a field. cannot be null.
 requiredField: whether or not filling this field is required for the event to be created.
-fieldTypeId: foreign key referencing fieldtypes. cannot be null.
+fieldType: 0 for basic type, 1 for user, 2 for group, 3 for file.
 Primary key is eventTypeId and fieldName together.*/';
 
 
@@ -325,27 +298,6 @@ COMMENT = 'Table describing which rooms are used for an event.\n\neventId /* com
 roomId: foreign key referencing rooms table
 primary key is both together.*/';
 
-
--- -----------------------------------------------------
--- Table `pksudb`.`etfstringlength`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `pksudb`.`etfstringlength` ;
-
-CREATE  TABLE IF NOT EXISTS `pksudb`.`etfstringlength` (
-  `eventTypeId` INT NOT NULL ,
-  `dbFieldName` VARCHAR(45) NOT NULL ,
-  `stringLength` INT NOT NULL ,
-  PRIMARY KEY (`eventTypeId`, `dbFieldName`) ,
-  CONSTRAINT `strlengtheventtypeid`
-    FOREIGN KEY (`eventTypeId` , `dbFieldName` )
-    REFERENCES `pksudb`.`eventtypefields` (`eventTypeId` , `dbFieldName` )
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-COMMENT = 'Table for lengths of strings in fields of event types.\n\neven /* comment truncated */ /*tTypeId: Primary and foreign key along with dbFieldName.
-dbFieldName: Primary and foreign key along with eventTypeId.
-stringLength: Length of string, e.g. VARCHAR(45).*/';
-
 USE `pksudb` ;
 
 
@@ -358,30 +310,6 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `pksudb`;
-INSERT INTO `pksudb`.`eventtypes` (`eventTypeId`, `eventTypeName`, `dbTableName`) VALUES (1, 'Basic event', 'events');
-
-COMMIT;
-
--- -----------------------------------------------------
--- Data for table `pksudb`.`groups`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `pksudb`;
-INSERT INTO `pksudb`.`groups` (`groupId`, `groupName`) VALUES (1, 'Administrator');
-
-COMMIT;
-
--- -----------------------------------------------------
--- Data for table `pksudb`.`fieldtypes`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `pksudb`;
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (1, 'Integer');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (2, 'String');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (3, 'Boolean');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (4, 'Date and time');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (5, 'User');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (6, 'Group');
-INSERT INTO `pksudb`.`fieldtypes` (`fieldId`, `fieldtype`) VALUES (7, 'File');
+INSERT INTO `pksudb`.`eventtypes` (`eventTypeId`, `eventTypeName`) VALUES (1, 'Basic event');
 
 COMMIT;
--- Database/database_testdata.sql
+++ Database/database_testdata.sql
@@ -6,51 +6,34 @@ INSERT INTO rooms (roomName) VALUES ("Room 5");
 INSERT INTO rooms (roomName) VALUES ("Room 6");
 INSERT INTO rooms (roomName) VALUES ("Room 7");
 
-INSERT INTO groups (groupName) VALUES ("ConcertBookers");
-INSERT INTO groups (groupName) VALUES ("PokerPlayers");
+INSERT INTO users (userName, password, realName, admin, email, active, needsApproval) VALUES ("johan_the_man","johanersej","Johan", 1,"johan@stengade.dk",1,0);
+INSERT INTO users (userName, password, realName, admin, email, active, needsApproval) VALUES ("andreas_PKSU","andreasercool","Andreas", 1,"andreas@stengade.dk",1,0);
+INSERT INTO users (userName, password, realName, admin, email, active, needsApproval) VALUES ("stephan_kerbal","kerbaltillinux","Stephan", 1,"stephan@stengade.dk",1,0);
 
-INSERT INTO users (userName, password, realName, email, active, needsApproval) 
-	VALUES ("johan_the_man","johanersej","Johan","johan@stengade.dk",1,0);
-INSERT INTO users (userName, password, realName, email, active, needsApproval) 
-	VALUES ("andreas_PKSU","andreasercool","Andreas","andreas@stengade.dk",1,0);
-INSERT INTO users (userName, password, realName, email, active, needsApproval) 
-	VALUES ("stephan_kerbal","kerbaltillinux","Stephan","stephan@stengade.dk",1,0);
+INSERT INTO eventtypes (eventTypeName) VALUES ("Concert");
+INSERT INTO eventtypefields (eventTypeId, fieldName, fieldDescription, requiredField, fieldType) VALUES (2, "Band-name","Name of the band that's playing",1,0);
 
-INSERT INTO groupmembers (groupId,userId) VALUES (2,1);
-INSERT INTO groupmembers (groupId,userId) VALUES (1,3);
-INSERT INTO groupmembers (groupId,userId) VALUES (2,3);
-
-INSERT INTO eventtypes (eventTypeName, dbTableName) VALUES ("Concert","concert");
-INSERT INTO eventtypefields 
-	(eventTypeId, dbFieldName, fieldDescription, requiredField, fieldTypeId) 
-	VALUES (2, "band","band-name",1,2);
-
-CREATE TABLE pksudb.concert (
+CREATE TABLE pksudb.table_1 (
 	eventId int NOT NULL,
 	band VARCHAR(50) NOT NULL,
 	PRIMARY KEY (eventId),
-    CONSTRAINT concerteventid
+    CONSTRAINT table_1eventid
     FOREIGN KEY (eventId)
     REFERENCES pksudb.events (eventId)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
 
-INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) 
-	VALUES (3,2,"Muse","2013-03-19 22:00:00","2013-03-20 02:30:00",true,0);
+INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) VALUES (3,2,"Muse","2013-03-19 22:00:00","2013-03-20 02:30:00",true,0);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (1,2);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (1,3);
 
-INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) 
-	VALUES (1,1,"Poker","2013-03-22 22:00:00","2013-03-24 10:00:00",true,1);
+INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) VALUES (1,1,"Poker","2013-03-22 22:00:00","2013-03-24 10:00:00",true,1);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (2,4);
 
-INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) 
-	VALUES (2,1,"Kerbal","2013-03-02 22:00:00","2013-04-20 02:30:00",true,2);
+INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) VALUES (2,1,"Kerbal","2013-03-02 22:00:00","2013-04-20 02:30:00",true,2);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (3,1);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (3,6);
-INSERT INTO concert (eventId, band) VALUES (1, "muse");
 
-INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) 
-	VALUES (2,1,"MineCon 2013","2013-03-02 10:00:00","2013-03-02 18:30:00",true,1);
+INSERT INTO events (userId, eventTypeId, eventName, eventStart, eventEnd, visible, state) VALUES (2,1,"MineCon 2013","2013-03-02 10:00:00","2013-03-02 18:30:00",true,1);
 INSERT INTO eventroomsused (eventId, roomId) VALUES (4,5);
