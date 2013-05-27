9f8431515c26682d383a7fa7ad0ddfe3d204ef86
Johan V. Damgaard <pwr273@alumni.ku.dk>
2013-05-27 17:55:18 +0200

    group page works now, but is empty

---
+++ CalendarApplication/CalendarApplication/Controllers/GroupViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalendarApplication.Controllers
+{
+    public class GroupViewController : Controller
+    {
+        //
+        // GET: /GroupView/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
